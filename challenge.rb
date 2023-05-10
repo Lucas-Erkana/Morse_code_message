1d9a4189da5c1e299f86baf83215e898fa42c519
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-10 16:49:17 +0200

    Create a method to decode a Morse code character

--- challenge.rb
+++ challenge.rb
@@ -1,1 +1,12 @@
-2 + 2
+def morse_to_letter(morse)
+    letter = ''
+    morse_hash = {
+      a: '.-', b: '-...', c: '-.-.', d: '-..', e: '.',
+      f: '..-.', g: '--.', h: '....', i: '..', j: '.---',
+      k: '-.-', l: '.-..', m: '--', n: '-.', o: '---',
+      p: '.--.', q: '--.-', r: '.-.', s: '...', t: '-',
+      u: '..-', v: '...-', w: '.--', x: '-..-', y: '-.--', z: '--..'
+    }
+    morse_hash.each { |key, value| letter = key.upcase.to_s if value == morse }
+    letter
+  end
