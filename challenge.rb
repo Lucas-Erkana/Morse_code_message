4642a93de935f1d0b10730cb1be6f7909fa1d66f
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-10 16:54:42 +0200

    Create a method to decode the entire message in Morse code

--- challenge.rb
+++ challenge.rb
@@ -1,5 +1,6 @@
-def morse_to_letter(morse)
-    letter = ''
+# Method to convert morse to character
+def morse_to_character(morse)
+    character = ''
     morse_hash = {
       a: '.-', b: '-...', c: '-.-.', d: '-..', e: '.',
       f: '..-.', g: '--.', h: '....', i: '..', j: '.---',
@@ -7,6 +8,22 @@ def morse_to_letter(morse)
       p: '.--.', q: '--.-', r: '.-.', s: '...', t: '-',
       u: '..-', v: '...-', w: '.--', x: '-..-', y: '-.--', z: '--..'
     }
-    morse_hash.each { |key, value| letter = key.upcase.to_s if value == morse }
-    letter
+    morse_hash.each { |key, value| character = key.upcase.to_s if value == morse }
+    character
   end
+  
+  def morse_to_word(phrase)
+    word = ''
+    phrase_array = phrase.split
+    phrase_array.each { |section| word += morse_to_character(section) }
+    word
+  end
+  
+  def morse_to_message(message)
+    sentence = ''
+    message_array = message.split('   ')
+    message_array.each { |section| sentence += " #{morse_to_word(section)}" }
+    sentence.strip
+  end
+  puts morse_to_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+  
